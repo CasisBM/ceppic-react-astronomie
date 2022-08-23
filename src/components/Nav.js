446456db99033f9d7ea335b4e7048b746ab01f80
Khazise <71380558+Khazise@users.noreply.github.com>
2022-08-23 12:04:12 +0200

    ajout component Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import { Link, Outlet } from "react-router-dom";
+
+export default function Nac() {
+  return (
+    <>
+      <nav>
+        <Link to="/article">Articles</Link>
+        <Link to="/ajoutArticle">Ajouter Article</Link>
+        <Link to="/aPropos">A Propos</Link>
+      </nav>
+      <Outlet/>
+    </>
+  );
+}
